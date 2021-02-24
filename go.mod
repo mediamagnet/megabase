9ba68ddc8851e69345787dfe4610daaee1925795
mediamagnet <nathanec@gmail.com>
2021-02-24 13:22:58 -0600

    initial push

---
+++ go.mod
@@ -0,0 +1,13 @@
+module megabase
+
+go 1.15
+
+require (
+	fyne.io/fyne/v2 v2.0.0
+	github.com/go-flutter-desktop/hover v0.46.2 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.8.0
+	github.com/spf13/cobra v1.1.3 // indirect
+	golang.org/x/net v0.0.0-20210220033124-5f55cee0dc0d // indirect
+	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43 // indirect
+)
---
+++ megabase.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+	"github.com/sirupsen/logrus"
+	"image/color"
+	"os"
+)
+
+var log = &logrus.Logger{
+	Out:       os.Stderr,
+	Formatter: new(logrus.TextFormatter),
+	Hooks:     make(logrus.LevelHooks),
+	Level:     logrus.InfoLevel,
+}
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Megabase")
+
+	tabs := container.NewAppTabs(
+		container.NewTabItem("Installed", widget.NewLabel("Installed Here")),
+		container.NewTabItem("Download", widget.NewLabel("Downloads Here")),
+		container.NewTabItem("Mods", widget.NewLabel("Mod Downloads")),
+	)
+
+	rect := canvas.NewRectangle(color.White)
+
+	buttons := container.New(
+		layout.NewVBoxLayout(),
+		widget.NewButton("Login", func() {log.Infoln("Clicked")}),
+		widget.NewButton("New Instance", func() {log.Infoln("Clicked")}),
+		widget.NewButton("Delete Instance", func() {log.Infoln("Clicked")}),
+		widget.NewButton("Path", func() {filebox(w)}),
+		)
+
+	tabs.SetTabLocation(container.TabLocationTop)
+
+	rect.Resize(fyne.NewSize(150,150))
+	w.Resize(fyne.NewSize(300,500))
+	w.SetContent(container.New(layout.NewHBoxLayout(), container.NewVBox(tabs, rect), buttons))
+	w.ShowAndRun()
+}
+
+func filebox(window fyne.Window) {
+	dialog.ShowFolderOpen(func(dir fyne.ListableURI, err error) {
+		if err != nil {
+			dialog.ShowError(err, window)
+			return
+		}
+		return
+	}, window)
+}
+
